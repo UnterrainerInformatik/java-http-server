eb44252e5b6868a9106c68f769fd2ea67a48496d
Gerald Unterrainer <gu_falcon@hotmail.com>
2020-04-25 21:25:44 +0200

    update dependencies, fix db-access

--- src/test/java/info/unterrainer/commons/httpserver/scripts/jpas/TestJpa.java
+++ src/test/java/info/unterrainer/commons/httpserver/scripts/jpas/TestJpa.java
@@ -4,12 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import info.unterrainer.commons.rdbutils.entities.BasicEntity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
 
-@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder(toBuilder = true)
+@Accessors(fluent = true, chain = true)
+@Entity
 @Table(name = "test")
 public class TestJpa extends BasicEntity {
 
--- src/test/java/info/unterrainer/commons/httpserver/scripts/jsons/TestJson.java
+++ src/test/java/info/unterrainer/commons/httpserver/scripts/jsons/TestJson.java
@@ -1,15 +1,19 @@
 package info.unterrainer.commons.httpserver.scripts.jsons;
 
-import info.unterrainer.commons.httpserver.jsons.BasicJson;
+import info.unterrainer.commons.serialization.jsons.BasicJson;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder(toBuilder = true)
 @EqualsAndHashCode(callSuper = true)
+@Accessors(fluent = true, chain = true)
 public class TestJson extends BasicJson {
 
 	private String message;
