823a1afd3e5c6c708dea57adcd34b757fd646170
Psilo <psilo@unterrainer.info>
2020-07-17 10:51:33 +0200

    add localdatetime in handlerutils

--- src/main/java/info/unterrainer/commons/httpserver/HandlerUtils.java
+++ src/main/java/info/unterrainer/commons/httpserver/HandlerUtils.java
@@ -1,11 +1,18 @@
 package info.unterrainer.commons.httpserver;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import info.unterrainer.commons.httpserver.enums.QueryField;
 import info.unterrainer.commons.httpserver.exceptions.BadRequestException;
 import info.unterrainer.commons.httpserver.jsons.ListJson;
+import info.unterrainer.commons.serialization.JsonMapper;
 import io.javalin.http.Context;
 
 public class HandlerUtils {
+
+	public DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(JsonMapper.DATETIME_PATTERN);
 
 	public <J> void setPaginationParamsFor(final ListJson<J> jList, final long offset, final long pageSize,
 			final long count, final Context ctx) {
@@ -68,6 +75,32 @@ public class HandlerUtils {
 		String result = defaultValue;
 		if (o != null)
 			result = o;
+		else if (mandatory)
+			throw new BadRequestException(String.format("Parameter %s is mandatory", name));
+		return result;
+	}
+
+	public LocalDateTime getQueryParamAsDateTime(final Context ctx, final String name) {
+		return getQueryParamAsDateTime(ctx, name, true, null);
+	}
+
+	public LocalDateTime getQueryParamAsDateTime(final Context ctx, final String name,
+			final LocalDateTime defaultValue) {
+		return getQueryParamAsDateTime(ctx, name, false, defaultValue);
+	}
+
+	private LocalDateTime getQueryParamAsDateTime(final Context ctx, final String name, final boolean mandatory,
+			final LocalDateTime defaultValue) {
+		String o = ctx.queryParam(name);
+		LocalDateTime result = defaultValue;
+
+		if (o != null)
+			try {
+				result = LocalDateTime.parse(o, dateTimeFormatter);
+			} catch (DateTimeParseException e) {
+				throw new BadRequestException(String.format("Parameter %s has to be a dateTime of format '%s'", name,
+						JsonMapper.DATETIME_PATTERN));
+			}
 		else if (mandatory)
 			throw new BadRequestException(String.format("Parameter %s is mandatory", name));
 		return result;
--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2020-07-14T13:49:38Z";
-	public static final String pomVersion = "0.0.26";
+	public static final String buildTime = "2020-07-17T08:51:17Z";
+	public static final String pomVersion = "0.0.27";
 }
