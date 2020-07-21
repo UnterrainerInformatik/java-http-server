092c6f67fa209e5ebba2d5db1469778282a3397a
Psilo <psilo@unterrainer.info>
2020-07-21 13:56:25 +0200

    repair jetty version

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2020-07-21T11:41:24Z";
-	public static final String pomVersion = "0.0.29";
+	public static final String buildTime = "2020-07-21T11:55:58Z";
+	public static final String pomVersion = "0.0.30";
 }
