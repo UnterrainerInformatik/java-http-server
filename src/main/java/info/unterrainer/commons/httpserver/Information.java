148441eca7fdf28993947ba4093e371298411b63
Psilo <psilo@unterrainer.info>
2020-07-30 18:11:52 +0200

    version bump

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2020-07-30T12:11:20Z";
-	public static final String pomVersion = "0.0.38";
+	public static final String buildTime = "2020-07-30T16:11:03Z";
+	public static final String pomVersion = "0.0.39";
 }
