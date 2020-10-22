601ea829107aec89d8eceab965c7737ac8227063
Psilo <psilo@unterrainer.info>
2020-10-22 10:22:29 +0200

    bump parent pom version and remove old junit from classpath

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2020-10-22T08:06:41Z";
-	public static final String pomVersion = "0.1.5";
+	public static final String buildTime = "2020-10-22T08:20:38Z";
+	public static final String pomVersion = "0.1.6";
 }
