7a02b35e59c21d4fea8d1cfe12c5f277a4cc29ec
Psilo <psilo@unterrainer.info>
2020-12-15 13:37:37 +0100

    implement ParamMap.clone

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2020-12-10T07:26:30Z";
-	public static final String pomVersion = "0.1.11";
+	public static final String buildTime = "2020-12-14T14:13:09Z";
+	public static final String pomVersion = "0.1.12";
 }
--- src/main/java/info/unterrainer/commons/httpserver/daos/ParamMap.java
+++ src/main/java/info/unterrainer/commons/httpserver/daos/ParamMap.java
@@ -1,5 +1,6 @@
 package info.unterrainer.commons.httpserver.daos;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
@@ -14,4 +15,9 @@ public class ParamMap {
 
 	@Singular
 	private Map<String, Object> parameters;
+
+	@Override
+	protected ParamMap clone() throws CloneNotSupportedException {
+		return new ParamMap(new HashMap<String, Object>(parameters));
+	}
 }
