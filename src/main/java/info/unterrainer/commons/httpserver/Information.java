ca6499000401a6785837d40fa10661061828bb79
Psilo <psilo@unterrainer.info>
2021-01-22 09:07:03 +0100

    version bump for nexus staging

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2021-01-21T17:09:29Z";
-	public static final String pomVersion = "0.1.20";
+	public static final String buildTime = "2021-01-22T08:06:36Z";
+	public static final String pomVersion = "0.1.21";
 }
