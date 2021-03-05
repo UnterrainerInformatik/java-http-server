43491108713404664707f8160861bc1450cc850c
Gerald Unterrainer <gu_falcon@hotmail.com>
2021-03-05 09:17:40 +0100

    build

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2021-01-22T08:06:36Z";
-	public static final String pomVersion = "0.1.21";
+	public static final String buildTime = "2021-03-05T08:17:16Z";
+	public static final String pomVersion = "0.1.22";
 }
