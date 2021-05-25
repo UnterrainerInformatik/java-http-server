363a66c3dff44f1becdecdf9ab43a9108edf9897
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2021-05-25 15:55:19 +0200

    fix possible npe in BasicJpqlDao
    bump version

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++ src/main/java/info/unterrainer/commons/httpserver/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.httpserver;
 
 public class Information {
 	public static final String name = "Http-Server";
-	public static final String buildTime = "2021-03-10T08:45:24Z";
-	public static final String pomVersion = "0.1.24";
+	public static final String buildTime = "2021-05-18T06:57:49Z";
+	public static final String pomVersion = "0.2.8";
 }
