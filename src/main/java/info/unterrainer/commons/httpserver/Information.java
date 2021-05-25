93dc0db2fa5475900c21499c3a52a0d283e24a61
Gerald Unterrainer <gerald.unterrainer@cms-building.at>
2021-05-25 15:58:41 +0200

    gitignore Information.java

--- src/main/java/info/unterrainer/commons/httpserver/Information.java
+++
@@ -1,7 +0,0 @@
-package info.unterrainer.commons.httpserver;
-
-public class Information {
-	public static final String name = "Http-Server";
-	public static final String buildTime = "2021-05-18T06:57:49Z";
-	public static final String pomVersion = "0.2.8";
-}
